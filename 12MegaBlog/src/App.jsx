dd1144b6a390243e30ef7efbcac731d8333cd3c8
Harsh Rohra <rohrah31@gmail.com>
2025-07-26 13:55:45 +0530

    commit

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,43 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from "./store/authSlice"
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return loading ? (
+    <div className='min-h-screen flex items-center justify-center'>
+      <span>Loading...</span>
+    </div>
+  ) : (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from '../config.js';
+import conf from '../conf/conf.js';
 
 import { Client, Account, ID } from "appwrite";
 
@@ -46,8 +46,6 @@ export class AuthService {
             console.error("Error getting current user:", error);
             throw error;
         }
-
-        return null;
     }
 
     async logout() {
@@ -62,7 +60,7 @@ export class AuthService {
 
 }
 
-const AuthService = new AuthService();
+const authService = new AuthService();
 
-export default AuthService
+export default authService;
 
